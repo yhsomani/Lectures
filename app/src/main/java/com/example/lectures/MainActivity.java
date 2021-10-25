37d591ba2c80cb01f24708b9a4dbce57b30641aa
Yash Somani <ysomani07@gmail.com>
2021-10-26 01:34:47 +0530

    with some bug

--- app/src/main/java/com/example/lectures/MainActivity.java
+++ app/src/main/java/com/example/lectures/MainActivity.java
@@ -2,58 +2,53 @@ package com.example.lectures;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        bottomNavigationView.setSelectedItemId(R.id.Home);
-
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
+    }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
-    HomeFragment homeFragment = new HomeFragment();
-    HomeworkFragment homeworkFragment = new HomeworkFragment();
-    ExaminationFragment examinationFragment = new ExaminationFragment();
-    NoticeFragment noticeFragment = new NoticeFragment();
-    AccountFragment accountFragment = new AccountFragment();
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.Home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-                return true;
-
-            case R.id.Homework:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeworkFragment).commit();
-                return true;
-
-            case R.id.Examination:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, examinationFragment).commit();
-                return true;
-
-            case R.id.Notice:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, noticeFragment).commit();
-                return true;
-
-            case R.id.Account:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, accountFragment).commit();
-                return true;
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.Home:
+                    openFragment(HomeFragment.newInstance("", ""));
+                    return true;
+                case R.id.Homework:
+                    openFragment(HomeworkFragment.newInstance("", ""));
+                    return true;
+                case R.id.Examination:
+                    openFragment(ExaminationFragment.newInstance("", ""));
+                    return true;
+                case R.id.Notice:
+                    openFragment(NoticeFragment.newInstance("", ""));
+                    return true;
+                case R.id.Account:
+                    openFragment(AccountFragment.newInstance("", ""));
+                    return true;
+            }
+            return false;
         }
-        return false;
-    }
+    };
 }
